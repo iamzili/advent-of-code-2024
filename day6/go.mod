6791762727de39811abd6d941743c491911289ca
zili <erik.zilinsky@gmail.com>
2025-03-02 18:06:58 +0100

    part2 added, not mine

--- day6/go.mod
+++ day6/go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.23.4
+
+require github.com/shraddhaag/aoc v0.0.0-20250101133214-ae46e8fbf1c3 // indirect
--- day6/main.go
+++ day6/main.go
@@ -2,84 +2,66 @@ package main
 
 import (
 	"fmt"
+	"image"
 	"os"
-	"slices"
+	"strings"
 )
 
-const obstacle, guard, eol byte = 35, 94, 10
+// Credit: https://github.com/mnml/aoc/tree/main/2024
 
 func main() {
-	input, _ := os.ReadFile("input")
-	size := slices.Index(input, eol)
-	currPos := slices.Index(input, guard)
-	//startPos := currPos
-	currDirection := "up"
-	// add the initial position
-	visited := map[int]int{
-		currPos: 1,
-	}
-	move := true
+	input, _ := os.ReadFile("input2")
 
-	for move {
-		switch {
-		case currDirection == "up":
-			currPos -= size + 1
-			if !indexExists(input, currPos) { // We found the way out
-				move = false
-				break
+	// grid = map[image.Point]int32 [
+	//     		{X: 0, Y: 0}: 46,
+	//     		{X: 1, Y: 0}: 46,
+	//			etc ...
+	// start = image.Point {X: 4, Y: 6}
+
+	grid, start := map[image.Point]rune{}, image.Point{}
+	for y, s := range strings.Fields(string(input)) {
+		for x, r := range s {
+			if r == '^' {
+				start = image.Point{x, y}
 			}
-			if isObstacle(input, currPos) { // start moving right
-				currDirection = "right"
-				currPos += size + 1 // Go back to the previous position
+			grid[image.Point{x, y}] = r
+		}
+	}
+
+	// delta = []image.Point len: 4, cap: 4, [
+	//    {X: 0, Y: -1},     up,    delta[0] aka 0
+	//    {X: 1, Y: 0},      right, delta[1] aka 1
+	//    {X: 0, Y: 1},      down,  delta[2] aka 2
+	//    {X: -1, Y: 0},     left,  delta[3] aka 3
+	//]
+
+	patrol := func(o image.Point) map[image.Point]int {
+		delta := []image.Point{{0, -1}, {1, 0}, {0, 1}, {-1, 0}}
+		// d contains the direction
+		p, d, seen := start, 0, map[image.Point]int{}
+		for {
+			if _, ok := grid[p]; !ok {
+				// found the exit
+				return seen
+			} else if 1<<d&seen[p] != 0 { // 1. seen[p] ensures that p needs to be in the map, if not then x & 0 = 0
+				// we found a loop
+				return nil
+			}
+			seen[p] |= 1 << d                                   // add a step to the "seen" map
+			if n := p.Add(delta[d]); grid[n] == '#' || n == o { // n == o needed for exit
+				d = (d + 1) % len(delta) // set new direction, e.g. if the curr direction is 0 which is up then: 1 % 4 = 1 aka right is going to be next direction
 			} else {
-				visited[currPos] += 1
-			}
-		case currDirection == "down":
-			currPos += size + 1
-			if !indexExists(input, currPos) {
-				fmt.Println("We found the way out!")
-				move = false
-				break
-			}
-			if isObstacle(input, currPos) { // start moving left
-				currDirection = "left"
-				currPos -= size + 1 //Go back to the previous position
-			} else {
-				visited[currPos] += 1
-			}
-		case currDirection == "right":
-			currPos += 1
-			if isObstacle(input, currPos) { // start moving down
-				currDirection = "down"
-				currPos -= 1 // Go back to the previous position
-			} else if input[currPos] == eol { // We found the way out
-				move = false
-				break
-			} else {
-				visited[currPos] += 1
-			}
-		case currDirection == "left":
-			currPos -= 1
-			// We found the way out
-			if !indexExists(input, currPos) || input[currPos] == eol {
-				move = false
-				break
-			}
-			if isObstacle(input, currPos) { // start moving up
-				currDirection = "up"
-				currPos += 1 //Go back to the previous position
-			} else {
-				visited[currPos] += 1
+				p = n
 			}
 		}
 	}
-	fmt.Println("distinct positions visited by the guard is ", len(visited))
-}
 
-func indexExists(slice []byte, index int) bool {
-	return index >= 0 && index < len(slice)
-}
-
-func isObstacle(slice []byte, index int) bool {
-	return slice[index] == obstacle
+	part1, part2 := patrol(image.Point{-1, -1}), 0
+	for p := range part1 {
+		if patrol(p) == nil {
+			part2++
+		}
+	}
+	fmt.Println(len(part1))
+	fmt.Println(part2)
 }
