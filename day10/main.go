873dd0affe2b20db915775b087f9f403a86c2bd4
iamzili <erik.zilinsky@gmail.com>
2025-07-02 19:29:45 +0200

    day10 part1

---
+++ day10/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"os"
+	"slices"
+	"strings"
+)
+
+func main() {
+	input, _ := os.ReadFile("input2")
+
+	// grid = map[image.Point]map[image.Point]bool [
+	//     		{X: 2, Y: 0}: [
+	//				{X: 3, Y: 0}: true,
+	//				{X: 2, Y: 1}: true,
+	//    	    ],
+	//          X: 2, Y: 5}: [
+	//          ]
+	//			etc ...
+
+	grid := map[image.Point]rune{}
+	trailheads := make(map[image.Point]map[image.Point]bool)
+	for y, s := range strings.Fields(string(input)) {
+		for x, r := range s {
+			if r == '0' {
+				trailheads[image.Point{x, y}] = make(map[image.Point]bool)
+			}
+			grid[image.Point{x, y}] = r
+		}
+	}
+
+	delta := []image.Point{{0, -1}, {1, 0}, {0, 1}, {-1, 0}}
+	candidates := []image.Point{}
+	score := 0
+
+	for tr := range trailheads {
+		currPos := tr
+		for { // check one starting point
+			for _, d := range delta { // check every direction at currPos to find a candidate
+				if n := currPos.Add(d); int(grid[n])-'0' == ((int(grid[currPos]) - '0') + 1) {
+					if int(grid[n])-'0' == 9 {
+						trailheads[tr][n] = true
+					} else if !slices.Contains(candidates, n) {
+						candidates = append(candidates, n)
+					}
+				}
+			}
+			if candidates == nil {
+				break // no candidates found, exit the loop
+			} else {
+				currPos = candidates[0] // take the first candidate as the new current position
+				if len(candidates) != 1 {
+					candidates = candidates[1:] // remove the first candidate from the list
+				} else {
+					candidates = nil
+				}
+			}
+		}
+	}
+	for _, val := range trailheads {
+		score += len(val)
+	}
+	fmt.Println("part1: ", score)
+
+}
